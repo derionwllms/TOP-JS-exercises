225d583696ff0ffb71ca36edb8a6ba5d4ec63b3e
Derion Williams <derionwllms@gmail.com>
2022-02-07 10:42:32 -0600

    Solved the first two JS practice exercises.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let output = ''
+    if(num < 0){
+        output = 'ERROR';
+        return(output);
+    }
+    else{
+        for(let x=0; x<num; x++){
+            output += string;
+        }
+        
+        return(output);
+    }
 };
 
 // Do not edit below this line
