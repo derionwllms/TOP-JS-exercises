49b23b17221688d1d9068dac6545ef59c9177420
Derion Williams <derionwllms@gmail.com>
2022-02-07 18:57:22 -0600

    finished the third exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    let len = input.length-1
+    let output = ""
+    for(x = len; x >=0; x--){
+        output += input[x]
+    }
+    return(output)
 };
 
 // Do not edit below this line
