b421b80d3f71b61f7d68ea8ce914d148c2a19729
Derion Williams <derionwllms@gmail.com>
2022-02-08 11:07:15 -0600

    finished the fourth exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...itemsToRemove) {
+    originalArr = arr
+    unwantedArr = itemsToRemove
+    len = unwantedArr.length-1
 
+    for(x = 0; x<=len; x++){
+       originalArr = originalArr.filter(function(item){
+                return item !== unwantedArr[x]
+            })
+    }
+    return(originalArr)
 };
+
+//console.log(removeFromArray([1, 2, 3, 4], 3))
+//removeFromArray([1, 2, 3, 4, 7], 7, "tacos")
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
